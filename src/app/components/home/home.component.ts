0e098ca13efb01f49873ecc59b7e53e46a88e9d7
szcleiton <souzacleiton54@gmail.com>
2024-08-15 00:23:56 -0300

    chore: using signals with template

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FooterModule } from '../footer/footer.module';
 
 @Component({
@@ -9,10 +9,20 @@ import { FooterModule } from '../footer/footer.module';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  name = "Cleiton";
+  name = signal("Cleiton");
+  count = 0;
   pacManUrl = "https://upload.wikimedia.org/wikipedia/commons/0/06/Pac_Man.svg";
 
+  constructor() {
+    setInterval(() => {
+      this.count += 1;
+    }, 1000)
+  }
+
   teste() {
-    console.log("teste");
+    this.count += 1;
+    // this.count.update(count => count + 1);
+    // this.name.set("Cleiton " + this.count());
+    console.log(this.count);
   }
 }
