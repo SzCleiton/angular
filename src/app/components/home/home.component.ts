342dd34dbe574aceca85ba1ac2769f87ebff629f
szcleiton <souzacleiton54@gmail.com>
2024-08-15 00:46:39 -0300

    feat: using new directives and old directives, and the form using

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,28 +1,35 @@
 import { Component, signal } from '@angular/core';
 import { FooterModule } from '../footer/footer.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterModule],
+  imports: [FooterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   name = signal("Cleiton");
+  //count = signal(0);
   count = 0;
+  value = false;
   pacManUrl = "https://upload.wikimedia.org/wikipedia/commons/0/06/Pac_Man.svg";
 
-  constructor() {
-    setInterval(() => {
-      this.count += 1;
-    }, 1000)
-  }
+  lista = ["fernanda", "cleiton", "joao", "maria"];
+
+  // constructor() {
+  //   setInterval(() => {
+  //     this.count += 1;
+  //   }, 1000)
+  // }
 
   teste() {
-    this.count += 1;
+    //this.count += 1;
     // this.count.update(count => count + 1);
     // this.name.set("Cleiton " + this.count());
-    console.log(this.count);
+    //console.log(this.count);
+    //this.count.update(value => value + 1);
+    this.count += 1;
   }
 }
